63084dd2fbdc2c587d46e8d393a47c8695ae59f3
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2022-11-22 17:40:06 -0300

    UPDATE

--- src/main/java/io/github/RafaGois/MyConfiguration.java
+++ src/main/java/io/github/RafaGois/MyConfiguration.java
@@ -1,15 +1,19 @@
 package io.github.RafaGois;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")//deixa acessivel apenas dquado o usuario for desenvolvimento
 public class MyConfiguration {
 
-    //adiciona este metodo no centexto da aplicacao
-    @Bean(name = "applicationName")
-    public String applicationName () {
-        return "Sistema de vendas";
+    @Bean
+    public CommandLineRunner executar () {
+        return args -> {
+            System.out.println("RODANDO A CONGIF DE DESENVOLVIMENTO");
+        };
     }
 
 }
